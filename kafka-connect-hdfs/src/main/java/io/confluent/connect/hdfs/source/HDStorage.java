9cab326502028e847442443288ed4f3a5767134a
fatema-sagar <fatema.sagar@hashedin.com>
2019-12-16 13:47:37 +0530

    Reading files from the hdfs url

--- kafka-connect-hdfs/src/main/java/io/confluent/connect/hdfs/source/HDStorage.java
+++ kafka-connect-hdfs/src/main/java/io/confluent/connect/hdfs/source/HDStorage.java
@@ -1,4 +1,123 @@
+/*
+ * Copyright [2019 - 2019] Confluent Inc.
+ */
+
 package io.confluent.connect.hdfs.source;
 
-public class HDStorage {
+import io.confluent.connect.cloud.storage.source.CloudSourceStorage;
+import io.confluent.connect.cloud.storage.source.StorageObject;
+import io.confluent.license.util.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * This has implementation of methods needed to interact with
+ * Hadoop File System.
+ */
+public class HDStorage implements CloudSourceStorage {
+
+    private static final Logger log = LoggerFactory.getLogger(HDStorage.class);
+    private static final String VERSION_FORMAT = "APN/1.0 Confluent/1.0 KafkaHDFSConnector/%s";
+    private final String url;
+    private final HDSourceConnectorConfig config;
+    private FileSystem fileSystem;
+    private Set<String> topicPartiton = new HashSet<>();
+    /**
+     *
+     * @param url HDFS address
+     * @param config HDFS Configuration
+     */
+    public HDStorage(HDSourceConnectorConfig config, String url) {
+        this.url = url;
+        this.config = config;
+    }
+
+    public String getUrl() { return url; }
+
+    public HDSourceConnectorConfig getConfig() { return config; }
+
+    /**
+     * @throws IOException
+     */
+    public Set<String> readFiles() throws IOException {
+        FileSystem fs = FileSystem.get(URI.create(config.getHdfsUrl()), new Configuration());
+        FileStatus[] fileStatus = fs.listStatus(new Path(config.getHdfsUrl()));
+        for(FileStatus status : fileStatus){
+            if (status.getPath().toString().contains(config.getTopicsFolder())) {
+                readFilesFromFolder(status.getPath().toString());
+            }
+        }
+        return topicPartiton;
+    }
+
+    private void readFilesFromFolder(String path) throws IOException {
+
+        FileSystem fileSystem = FileSystem.get(URI.create(path), new Configuration());
+        FileStatus[] status = fileSystem.listStatus(new Path(path));
+        for (FileStatus fileStatus : status) {
+            if ( !fileStatus.getPath().toString().contains("/+tmp")) {
+                countPartitions(fileStatus);
+            }
+        }
+    }
+
+    private void countPartitions(FileStatus fileStatus) throws IOException {
+        if (fileStatus.isDirectory()) {
+            FileSystem fs = FileSystem.get(URI.create(fileStatus.getPath().toString()), new Configuration());
+            FileStatus[] fst = fs.listStatus(new Path(fileStatus.getPath().toString()));
+            for (FileStatus status : fst) {
+                topicPartiton.add(status.getPath().toString());
+            }
+        }
+    }
+
+    @Override
+    public StorageObject getStorageObject(String key) {
+        return new HDStorageObject(fileSystem, key);
+    }
+
+    @Override
+    public List<StorageObject> getListOfStorageObjects(String path) {
+        throw new UnsupportedOperationException(
+                "Implementation not added in HDFS Storage class."
+        );
+    }
+
+    @Override
+    public boolean exists(String name) { return StringUtils.isNotBlank(name); }
+
+    @Override
+    public boolean bucketExists() {
+        return false;
+    }
+
+    @Override
+    public void delete(String name) {
+
+    }
+
+    @Override
+    public StorageObject open(String path) {
+        return null;
+    }
+
+    public void close(){
+    }
+
+    public String getNextFileName(String path, String startAfterThisFile, String fileSuffix) {
+        log.trace("Listing objects on hdfs with path {} starting after file {} and having suffix {}",
+                path, startAfterThisFile, fileSuffix
+        );
+        return null;
+    }
 }
--- kafka-connect-hdfs/src/main/java/io/confluent/connect/storage/partitioner/DefaultPartitioner.java
+++ kafka-connect-hdfs/src/main/java/io/confluent/connect/storage/partitioner/DefaultPartitioner.java
@@ -4,5 +4,65 @@
 
 package io.confluent.connect.storage.partitioner;
 
-public class DefaultPartitioner {
+import io.confluent.connect.hdfs.source.HDSourceConnectorConfig;
+import io.confluent.connect.hdfs.source.HDStorage;
+import org.apache.hadoop.fs.FileStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DefaultPartitioner implements Partitioner {
+    private static final Logger log = LoggerFactory.getLogger(DefaultPartitioner.class);
+
+    private final HDSourceConnectorConfig config;
+    private final HDStorage storage;
+    private Set<String> partitions = Collections.emptySet();
+    private final String extension;
+
+    public DefaultPartitioner(HDSourceConnectorConfig config, HDStorage storage) {
+        this.config = config;
+        this.storage = storage;
+        extension = config.getInstantiatedStorageObjectFormat().getExtension();
+    }
+
+    @Override
+    public Set<String> getPartitions() throws IOException {
+        partitions = calculatePartitions();
+        return partitions;
+    }
+
+    /**
+     * Calculates number of partitions from given topic folder.
+     * <p> Example : if file is located at -
+     * "topics/hdfs_topic/partition=0/hdfs_topic+0+0000000000.avro" then
+     * partition value will be "topics/gcs_topic/partition=0/" </p>
+     * @return : List of partitions.
+     */
+    private Set<String> calculatePartitions() {
+        Set<String> partitions = new HashSet<>();
+        try {
+            partitions = storage.readFiles();
+            log.debug("Got partitions {}", partitions);
+            return partitions;
+        } catch (IOException exception) {
+            log.error("Caught an IOException while fetching data");
+        }
+
+    }
+
+    public String getNextObjectName(String topic, String previousObject) {
+        log.trace("Get next object from topic {} using previous object {} and extension {}", topic,
+                previousObject, extension
+        );
+        return storage.getNextFileName(topic, previousObject, extension);
+    }
+
+    public boolean shouldReconfigure() {
+        return !partitions.equals((calculatePartitions()));
+    }
 }
